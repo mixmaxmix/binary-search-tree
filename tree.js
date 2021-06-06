7cf9ffb6e0aa694d34fd95c41ae83cc4626685ee
mixmaxmix <62526387+mixmaxmix@users.noreply.github.com>
2021-06-06 17:27:20 +0300

    Third commit

--- tree.js
+++ tree.js
@@ -30,7 +30,8 @@ class Tree {
     }
 
     search(val) {
-        
+        let found = this.root.search(val);
+        return found;
     }
 }
 
@@ -39,6 +40,17 @@ class Node {
         this.value = val;
         this.left = null;
         this.right = null;
+    }
+
+    search(val) {
+        if (this.value == val) {
+            return this;
+        } else if (val < this.value && this.left != null){
+            return this.left.search(val);
+        } else if (val > this.value && this.right != null){
+            return this.right.search(val);
+        }
+        return null;
     }
 
     visit() {
