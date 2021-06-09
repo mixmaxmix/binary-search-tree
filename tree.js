26718053b2f83288e64f6186513b5834772c5338
mixmaxmix <62526387+mixmaxmix@users.noreply.github.com>
2021-06-10 00:22:30 +0300

    Added animation, colors and "fixed" sticking

--- tree.js
+++ tree.js
@@ -1,167 +1,185 @@
 "use strict";
 
 document.addEventListener("keyup", (event) => {
-	if (event.code === "Space") {
-		document.querySelector(".message").style.display = "none";
-		setup();
-	}
+  if (event.code === "Space") {
+    document.querySelector(".message").style.display = "none";
+    setup();
+  }
 });
 
 function setup() {
-	const randomNumber = Math.round(Math.random() * 200 - 100);
-	tree.addValue(randomNumber);
+  const randomNumber = Math.round(Math.random() * 200 - 100);
+  tree.addValue(randomNumber);
 
-	tree.renderTree();
+  tree.renderTree();
 }
 
 let clickedNodeValue = null;
 
+const nodeContainerStyles = {
+    left: -250,
+    right: -250,
+    after: {
+    top: -100,
+    left: -98,
+    rotation: 73,
+    height: 201,
+  },
+};
+
 class Tree {
-	constructor() {
-		this.root = null;
-	}
+  constructor() {
+    this.root = null;
+  }
 
-	traverse() {
-		this.root.visit();
-	}
+  traverse() {
+    this.root.visit();
+  }
 
-	addValue(val) {
-		let n = new Node(val);
+  addValue(val) {
+    let n = new Node(val);
 
-		if (this.root === null) {
-			this.root = n;
-		} else {
-			this.root.addNode(n);
-		}
-	}
+    if (this.root === null) {
+      this.root = n;
+    } else {
+      this.root.addNode(n);
+    }
+  }
 
-	search(val) {
-		let found = this.root.search(val);
-		return found;
-	}
+  search(val) {
+    let found = this.root.search(val);
+    return found;
+  }
 
-	renderTree() {
-		this.renderNodes(this.root);
-	}
+  renderTree() {
+    this.renderNodes(this.root);
+  }
 
-	renderNodes(parentNodeObject, modifier = 200) {
-		if (!document.querySelector(".root")) {
-			const nodeContainer = document.createElement("div");
-			nodeContainer.className = "node root";
-			nodeContainer.id = parentNodeObject.value;
-			nodeContainer.innerHTML = parentNodeObject.value;
+  renderNodes(parentNodeObject, modifier = 1.5) {
+    if (!document.querySelector(".root")) {
+      const nodeContainer = document.createElement("div");
+      nodeContainer.className = "node root";
+      nodeContainer.id = parentNodeObject.value;
+      nodeContainer.innerHTML = parentNodeObject.value;
 
-			document.body.append(nodeContainer);
+      document.body.append(nodeContainer);
 
-			nodeContainer.addEventListener("click", (e) => {
-				clickedNodeValue = parentNodeObject.value;
+      nodeContainer.addEventListener("click", (e) => {
+        clickedNodeValue = parentNodeObject.value;
 
-				if (clickedNodeValue == e.target.id) {
-					document.getElementById(clickedNodeValue).outerHTML = "";
-					tree = new Tree();
-					document.querySelector(".message").style.display = "block";
-				}
-			});
+        if (clickedNodeValue == e.target.id) {
+          document.getElementById(clickedNodeValue).outerHTML = "";
+          tree = new Tree();
+          document.querySelector(".message").style.display = "block";
+        }
+      });
 
-			this.renderNodes(parentNodeObject);
-		}
+      this.renderNodes(parentNodeObject);
+    }
 
-		if (parentNodeObject.left !== null) {
-			if (!document.getElementById(parentNodeObject.left.value)) {
-				const nodeContainer = document.createElement("div");
-				nodeContainer.className = "node leftChild";
-				nodeContainer.id = parentNodeObject.left.value;
-				nodeContainer.innerHTML = parentNodeObject.left.value;
+    if (parentNodeObject.left !== null) {
+      if (!document.getElementById(parentNodeObject.left.value)) {
+        const nodeContainer = document.createElement("div");
+        nodeContainer.className = "node leftChild";
+        nodeContainer.id = parentNodeObject.left.value;
+        nodeContainer.innerHTML = parentNodeObject.left.value;
 
-				document
-					.getElementById(parentNodeObject.value)
-					.insertAdjacentElement("beforeend", nodeContainer);
+        nodeContainer.style.left = nodeContainerStyles.left / modifier + "px";
 
-				nodeContainer.addEventListener("click", (e) => {
-					clickedNodeValue = parentNodeObject.left.value;
 
-					if (clickedNodeValue == e.target.id) {
-						document.getElementById(clickedNodeValue).outerHTML = "";
-						parentNodeObject.left = null;
-					}
-				});
-			}
 
-			this.renderNodes(parentNodeObject.left, modifier + 10);
-		}
-		if (parentNodeObject.right !== null) {
-			if (!document.getElementById(parentNodeObject.right.value)) {
-				const nodeContainer = document.createElement("div");
-				nodeContainer.className = "node rightChild";
-				nodeContainer.id = parentNodeObject.right.value;
-				nodeContainer.innerHTML = parentNodeObject.right.value;
+        document
+          .getElementById(parentNodeObject.value)
+          .insertAdjacentElement("beforeend", nodeContainer);
 
-				document
-					.getElementById(parentNodeObject.value)
-					.insertAdjacentElement("beforeend", nodeContainer);
+        nodeContainer.addEventListener("click", (e) => {
+          clickedNodeValue = parentNodeObject.left.value;
 
-				nodeContainer.addEventListener("click", (e) => {
-					clickedNodeValue = parentNodeObject.right.value;
+          if (clickedNodeValue == e.target.id) {
+            document.getElementById(clickedNodeValue).outerHTML = "";
+            parentNodeObject.left = null;
+          }
+        });
+      }
 
-					if (clickedNodeValue == e.target.id) {
-						document.getElementById(clickedNodeValue).outerHTML = "";
-						parentNodeObject.right = null;
-					}
-				});
-			}
+      this.renderNodes(parentNodeObject.left, modifier * 1.5);
+    }
+    if (parentNodeObject.right !== null) {
+      if (!document.getElementById(parentNodeObject.right.value)) {
+        const nodeContainer = document.createElement("div");
+        nodeContainer.className = "node rightChild";
+        nodeContainer.id = parentNodeObject.right.value;
+        nodeContainer.innerHTML = parentNodeObject.right.value;
 
-			this.renderNodes(parentNodeObject.right, modifier + 10);
-		}
-	}
+        // nodeContainer.style.right = -modifier + "px";
+        nodeContainer.style.right = nodeContainerStyles.right / modifier + "px";
+
+        document
+          .getElementById(parentNodeObject.value)
+          .insertAdjacentElement("beforeend", nodeContainer);
+
+        nodeContainer.addEventListener("click", (e) => {
+          clickedNodeValue = parentNodeObject.right.value;
+
+          if (clickedNodeValue == e.target.id) {
+            document.getElementById(clickedNodeValue).outerHTML = "";
+            parentNodeObject.right = null;
+          }
+        });
+      }
+
+      this.renderNodes(parentNodeObject.right, modifier * 1.5);
+    }
+  }
 }
 
 class Node {
-	constructor(val) {
-		this.value = val;
-		this.left = null;
-		this.right = null;
-	}
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
 
-	getValue() {
-		return this.value;
-	}
+  getValue() {
+    return this.value;
+  }
 
-	search(val) {
-		if (this.value == val) {
-			return this;
-		} else if (val < this.value && this.left != null) {
-			return this.left.search(val);
-		} else if (val > this.value && this.right != null) {
-			return this.right.search(val);
-		}
-		return null;
-	}
+  search(val) {
+    if (this.value == val) {
+      return this;
+    } else if (val < this.value && this.left != null) {
+      return this.left.search(val);
+    } else if (val > this.value && this.right != null) {
+      return this.right.search(val);
+    }
+    return null;
+  }
 
-	visit() {
-		if (this.left != null) {
-			this.left.visit();
-		}
+  visit() {
+    if (this.left != null) {
+      this.left.visit();
+    }
 
-		if (this.right != null) {
-			this.right.visit();
-		}
-	}
+    if (this.right != null) {
+      this.right.visit();
+    }
+  }
 
-	addNode(n) {
-		if (n.value < this.value) {
-			if (this.left == null) {
-				this.left = n;
-			} else {
-				this.left.addNode(n);
-			}
-		} else if (n.value > this.value) {
-			if (this.right == null) {
-				this.right = n;
-			} else {
-				this.right.addNode(n);
-			}
-		}
-	}
+  addNode(n) {
+    if (n.value < this.value) {
+      if (this.left == null) {
+        this.left = n;
+      } else {
+        this.left.addNode(n);
+      }
+    } else if (n.value > this.value) {
+      if (this.right == null) {
+        this.right = n;
+      } else {
+        this.right.addNode(n);
+      }
+    }
+  }
 }
 
 let tree = new Tree();
